5ab271700eb727940443fc80ae01af7f0453e7d3
jiachaoxiong <2623484968@qq.com>
2026-06-01 12:11:24 +0800

    feat: Spring Boot 项目脚手架

---
+++ qichegaise/qcg-server/src/main/java/com/qcg/QcgApplication.java
@@ -0,0 +1,11 @@
+package com.qcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QcgApplication.class, args);
+    }
+}
